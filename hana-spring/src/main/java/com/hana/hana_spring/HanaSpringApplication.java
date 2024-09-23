4cd216eee26fc1b35747cc347cf840a330406d0f
ru <ru@fufu.moe>
2024-09-23 15:48:48 +0800

    init

---
+++ hana-spring/src/main/java/com/hana/hana_spring/HanaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.hana.hana_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanaSpringApplication.class, args);
+	}
+
+}
---
+++ hana-spring/src/test/java/com/hana/hana_spring/HanaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hana.hana_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
