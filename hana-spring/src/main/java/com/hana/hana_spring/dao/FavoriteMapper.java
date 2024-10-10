175a16b1453bf598ec610ecba48ad10646a35dde
ufoum <uf@fufu.moe>
2024-10-10 11:33:13 +0800

    添加收藏功能

---
+++ hana-spring/src/main/java/com/hana/hana_spring/dao/FavoriteMapper.java
@@ -0,0 +1,24 @@
+package com.hana.hana_spring.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
+
+import com.hana.hana_spring.entity.Resource;
+
+@Mapper
+public interface FavoriteMapper {
+    @Select("select resource.* from resource,favorite where favorite.uid=#{uid} and favorite.rid=resource.id")
+    List<Resource> sel_all(int uid);
+
+    @Insert("insert into favorite(uid, rid) values(#{uid}, #{rid})")
+    @Options(useGeneratedKeys = true, keyProperty = "id")
+    void ins(int uid, int rid);
+
+    @Delete("delete from favorite where uid=#{uid} and rid=#{rid}")
+    void del(int uid, int rid);
+}
---
+++ hana-spring/src/main/java/com/hana/hana_spring/service/FavoriteService.java
@@ -0,0 +1,29 @@
+package com.hana.hana_spring.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.hana.hana_spring.dao.FavoriteMapper;
+import com.hana.hana_spring.entity.Resource;
+
+@Service
+@Transactional(rollbackFor = Exception.class)
+public class FavoriteService {
+    @Autowired
+    private FavoriteMapper favorite_mapper;
+
+    public List<Resource> get_user_favorite(Integer uid) {
+        return favorite_mapper.sel_all(uid);
+    }
+
+    public void add_favorite(Integer uid, Integer rid) {
+        favorite_mapper.ins(uid, rid);
+    }
+
+    public void del_favorite(Integer uid, Integer rid) {
+        favorite_mapper.del(uid, rid);
+    }
+}
