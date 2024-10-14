6b5213509aa461cbdebfdd127c0f84bc9eafbe3e
ufoum <uf@fufu.moe>
2024-10-14 11:51:02 +0800

    修改上传错误

--- hana-spring/src/main/java/com/hana/hana_spring/utils/Oss.java
+++ hana-spring/src/main/java/com/hana/hana_spring/utils/Oss.java
@@ -24,9 +24,9 @@ import lombok.extern.slf4j.Slf4j;
 @NoArgsConstructor
 public class Oss {
     private String endpoint;
-    private String accessKeyId;
-    private String accessKeySecret;
-    private String bucketName;
+    private String username;
+    private String password;
+    private String bucket;
 
     public String upload(MultipartFile file) {
         // 生成UUID作为文件名
@@ -37,17 +37,17 @@ public class Oss {
         // 拼接新的文件名
         String object_name = uuid + extension;
 
-        OSS oss_client = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+        OSS oss_client = new OSSClientBuilder().build(endpoint, username, password);
 
         try {
             // 获取文件输入流
             InputStream input_stream = file.getInputStream();
 
             // 上传文件到指定的Bucket
-            oss_client.putObject(bucketName, object_name, input_stream);
+            oss_client.putObject(bucket, object_name, input_stream);
 
             // 返回文件的访问URL
-            return "https://" + bucketName + "." + endpoint + "/" + object_name;
+            return "https://" + bucket + "." + endpoint + "/" + object_name;
         } catch (IOException e) {
             log.error("上传失败！");
             return null;
@@ -61,11 +61,11 @@ public class Oss {
         String object_name = get_object_name_from_url(url);
 
         // 创建OSSClient实例
-        OSS oss_client = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+        OSS oss_client = new OSSClientBuilder().build(endpoint, username, password);
 
         try {
             // 删除文件
-            oss_client.deleteObject(bucketName, object_name);
+            oss_client.deleteObject(bucket, object_name);
         } catch (Exception e) {
             log.error("删除文件出错！");
         } finally {
