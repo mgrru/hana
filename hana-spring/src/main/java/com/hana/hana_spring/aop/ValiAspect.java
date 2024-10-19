fe1fc4dcc2b39f44e76fb45e8daedb9ae1b266bb
ufoum <uf@fufu.moe>
2024-10-20 02:32:52 +0800

    修复不进行权限验证的错误

--- hana-spring/src/main/java/com/hana/hana_spring/aop/ValiAspect.java
+++ hana-spring/src/main/java/com/hana/hana_spring/aop/ValiAspect.java
@@ -3,8 +3,9 @@ package com.hana.hana_spring.aop;
 import java.lang.reflect.Method;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.annotation.Order;
@@ -18,17 +19,25 @@ import com.hana.hana_spring.utils.JwtUtil;
 import com.hana.hana_spring.utils.Result;
 
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 
 @Aspect
 @Component
+@Slf4j
 @Async
 public class ValiAspect {
     @Autowired
     private JwtUtil jwt_util;
 
+    @Pointcut("@annotation(com.hana.hana_spring.anno.Validate) || @within(com.hana.hana_spring.anno.Validate)")
+    public void validate() {
+    }
+
     @Order(1)
-    @Before("@annotation(com.hana.hana_spring.anno.Validate) || @within(com.hana.hana_spring.anno.Validate)")
+    @Around("validate()")
     public Object login_validate(ProceedingJoinPoint join_point) throws Throwable {
+        log.info("登录验证");
+
         // 检查是否需要验证
         Method method = ((MethodSignature) join_point.getSignature()).getMethod();
         Validate validate = method.getAnnotation(Validate.class);
@@ -48,8 +57,10 @@ public class ValiAspect {
     }
 
     @Order(2)
-    @Before("@annotation(com.hana.hana_spring.anno.Validate) || @within(com.hana.hana_spring.anno.Validate)")
+    @Around("validate()")
     public Object admin_validate(ProceedingJoinPoint join_point) throws Throwable {
+        log.info("管理员验证");
+
         // 检查是否需要验证
         Method method = ((MethodSignature) join_point.getSignature()).getMethod();
         Validate validate = method.getAnnotation(Validate.class);
