64aab15840f72208dca85ba8e329c949b0b587b2
tangzhengtao <1399159010@qq.com>
2024-10-23 17:08:05 +0800

    收藏视频功能完善

--- hana-vue/src/store/favorite.js
+++ hana-vue/src/store/favorite.js
@@ -1,37 +1,46 @@
+/*
+ * @Author: tangzhengtao 1399159010@qq.com
+ * @Date: 2024-10-09 10:48:54
+ * @LastEditors: tangzhengtao 1399159010@qq.com
+ * @LastEditTime: 2024-10-22 22:04:27
+ * @FilePath: \vue3-app1\src\store\favorite.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { defineStore } from "pinia";
 import { ref } from "vue";
+import axios from "../utils/axios";
+import { ElMessage } from "element-plus";
 
-export const useFavoriteStore = defineStore("FavoriteStore", () => {
-  const favoriteList = ref([]);
+export const useFavoriteStore = defineStore("favoriteStore", () => {
+  const favoriteList = ref([]); // 收藏列表
 
-  const fetchFavoriteAnimes = async () => {
+  // 获取收藏列表
+  const fetchFavorites = async () => {
     try {
       const response = await axios.get("/favorites");
-      console.log("接口响应数据:", response.data); // 检查接口返回的数据结构
       favoriteList.value = JSON.parse(response.data);
     } catch (error) {
-      console.error("获取收藏视频列表失败：", error);
+      console.error("获取收藏列表失败：", error);
     }
   };
 
-  const addFavorite = async (video) => {
+  // 取消收藏
+  const removeFavorite = async (rid) => {
     try {
-      // 发送添加历史记录的请求
-      await axios.post(`/history/${anime.id}`, anime);
-      historyList.value.push(anime); // 更新本地记录
+      await axios.delete(`/favorites/${rid}`);
+      favoriteList.value = favoriteList.value.filter((item) => item.id !== rid);
+      ElMessage({
+        message: "取消收藏成功！",
+        type: "success",
+      });
     } catch (error) {
-      console.error("添加历史记录失败：", error);
+      console.error("取消收藏失败：", error);
     }
-  };
-
-  const removeFavorite = (id) => {
-    favoriteList.value = favoriteList.value.filter((item) => item.id !== id);
   };
 
   return {
-    fetchFavoriteAnimes,
     favoriteList,
-    addFavorite,
+    fetchFavorites,
     removeFavorite,
   };
 });
